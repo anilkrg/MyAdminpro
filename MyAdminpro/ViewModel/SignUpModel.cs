e7142ef3807360e811864a1c5265f4f462edfbb0
anilkrg <anil007gupta2012@gmail.com>
2022-03-16 14:56:08 +0530

    signup form work

--- MyAdminpro/ViewModel/SignUpModel.cs
+++ MyAdminpro/ViewModel/SignUpModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using MyAdminpro.ViewModel;
 
 namespace MyAdminpro.ViewModel
 {
     public class SignUpModel
     {
+        [Required]
         public string Fname { get; set; }
+        [Required]
         public string Lname { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
+        [Compare("Password",ErrorMessage ="password")]
         public string ConfirmPassword { get; set; }
         public string Gender { get; set; }
         
